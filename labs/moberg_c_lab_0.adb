cb2004d29f84c4d8624d5faf7e5648baa046b972
Cameron Moberg <cjmoberg@gmail.com>
2015-10-02 14:02:48 -0500

    Create moberg_c_lab_0.adb

---
+++ labs/moberg_c_lab_0.adb
@@ -0,0 +1,11 @@
+-- Cameron Moberg
+-- A simple program to display a message.
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Moberg_Lab_0 is
+
+begin --Moberg_Lab_0
+      Put_Line(Item=>"Hello, World!");
+end Moberg_Lab_0;
