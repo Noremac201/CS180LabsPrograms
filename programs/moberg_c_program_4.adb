4f620efcae7e728eec2a6ed9bdd4e7960430bc28
Cameron Moberg <cjmoberg@gmail.com>
2015-10-26 13:38:19 -0500

    Update moberg_c_program_4.adb

--- programs/moberg_c_program_4.adb
+++ programs/moberg_c_program_4.adb
@@ -17,9 +17,10 @@ use Ada.Characters.Latin_1;
 
 procedure moberg_c_program_4 is
 
-  type Baseball_Array_Type is array (Integer range 1..20,
-                                     Positive range 1..100) of Integer;
+  type Baseball_Array_Type is array (Positive range <>,
+                                     Positive range <>) of Integer;
   --Assign named values to array's memory slots for x axis.
+  --They are global because constants and in both procedures.
   Player_Number : Natural := 1;
   Hits : Natural := 2;
   Walks : Natural := 3;
@@ -28,21 +29,22 @@ procedure moberg_c_program_4 is
 
   procedure File_Check(Data_File : in out File_Type;
                        Player : out Baseball_Array_Type) is
-
    File_Name : constant String := "batting_stats.txt";
    Player_Count : Natural;
-   Actual_Hits :Integer;
+   Actual_Hits : Integer;
    Actual_Walks : Integer;
    Actual_Outs : Integer;
- begin --File_Check
 
-   Open (File => Data_File, Mode => In_File, Name => File_Name);
-   --Initialize the player numbers.
-   for i in 1..20 loop
-     Player(Player_Number,i) := i;
-   end loop;
+  begin --File_Check
+
+    Open (File => Data_File, Mode => In_File, Name => File_Name);
+    --Initialize the player numbers.
+    for i in 1..20 loop
+      Player(Player_Number,i) := i;
+    end loop;
 
    while not End_Of_File (File => Data_File) loop
+
       while not End_Of_Line (File => Data_File) loop
         --Initialize the readings
          Actual_Hits := 0;
@@ -59,47 +61,48 @@ procedure moberg_c_program_4 is
 
          Ada.Integer_Text_IO.Get (File => Data_File, Item => Actual_Outs);
          Player(Outs, Player_Count) := Player(Outs, Player_Count) + Actual_Outs;
-
       end loop;
 	 --Skips to next line to read more data.
    Skip_Line (File => Data_File);
    end loop;
-   Close (File => Data_File);
- end File_Check;
+  Close (File => Data_File);
+  end File_Check;
 
-procedure Output_Data(Player : in Baseball_Array_Type) is
+  procedure Output_Data(Player : in Baseball_Array_Type) is
+
     Batting_Average : Float;
 
-  begin --Output_Data
-   Put("Player #");
-   Ada.Text_IO.Set_Col(13);
-   Put("Bat Avg");
-   Ada.Text_IO.Set_Col(24);
-   Put("Walks");
-   New_Line;
-   Put("--------");
-   Ada.Text_IO.Set_Col(13);
-   Put("-------");
-   Ada.Text_IO.Set_Col(24);
-   Put("-----");
-   New_Line;
-   for Player_Count in 1..20 loop
-   Batting_Average := float(Player(Hits,Player_Count)) / float(Player(Hits,Player_Count) + Player(Outs,Player_Count));
-   Ada.Integer_Text_IO.Put(Item => Player(Player_Number,Player_Count), width => 4);
-   Ada.Text_IO.Set_Col(13);
-   Ada.Float_Text_IO.Put(Item => Batting_Average,
-                         fore => 2,
-                         aft => 3,
-                         exp => 0);
-   Ada.Text_IO.Set_Col(24);
-   Ada.Integer_Text_IO.Put(Item => Player(Walks, Player_Count), width => 3);
-   New_Line;
- end loop;
- end Output_Data;
+    begin --Output_Data
+       Put("Player #");
+       Ada.Text_IO.Set_Col(13);
+       Put("Bat Avg");
+       Ada.Text_IO.Set_Col(24);
+       Put("Walks");
+       New_Line;
+       Put("--------");
+       Ada.Text_IO.Set_Col(13);
+       Put("-------");
+       Ada.Text_IO.Set_Col(24);
+       Put("-----");
+       New_Line;
+       for Player_Count in 1..20 loop
+           Batting_Average := float(Player(Hits,Player_Count)) / float(Player(Hits,Player_Count) + Player(Outs,Player_Count));
+           Ada.Integer_Text_IO.Put(Item => Player(Player_Number,Player_Count), width => 4);
+           Ada.Text_IO.Set_Col(13);
+           Ada.Float_Text_IO.Put(Item => Batting_Average,
+                                 fore => 2,
+                                 aft => 3,
+                                 exp => 0);
+            Ada.Text_IO.Set_Col(24);
+            Ada.Integer_Text_IO.Put(Item => Player(Walks, Player_Count), width => 3);
+            New_Line;
+       end loop;
+  end Output_Data;
+
  Data_File : File_Type;
- Player : Baseball_Array_Type;
+ Player : Baseball_Array_Type(Player_Number..At_Bat,1..100) := (others => (others=>0));
 
 begin -- moberg_c_program_4
-File_Check(Data_File=>Data_File, Player=> Player);
-Output_Data(Player => Player);
+  File_Check(Data_File => Data_File, Player => Player);
+  Output_Data(Player => Player);
 end moberg_c_program_4;
