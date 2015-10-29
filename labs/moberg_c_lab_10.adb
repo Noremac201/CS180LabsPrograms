6cc2ecf6e5b271cb1a4638487cf41fe9f798010a
Cameron Moberg <cjmoberg@gmail.com>
2015-10-29 14:38:35 -0500

    Update moberg_c_lab_10.adb

--- labs/moberg_c_lab_10.adb
+++ labs/moberg_c_lab_10.adb
@@ -3,10 +3,14 @@ use Ada.Text_IO;
 with Ada.Float_Text_IO;
 use Ada.Float_Text_IO;
 
-procedure Lastname_I_Lab_10 is
+procedure moberg_c_lab_10 is
 
    --------------------------------------------------------------
-   -- Banner comment here
+   -- Cameron Moberg
+   -- 10/29/2015
+   -- Purpose: To use records and arrays to keep info on students.
+   -- Input: Text file name as well as the text file.
+   -- Output: The formatted Banner ID, Name, and GPA of the student.
    --------------------------------------------------------------
 
    subtype Name_Type is String (1 .. 30);
@@ -39,12 +43,14 @@ procedure Lastname_I_Lab_10 is
       Open (File => Data_File,
             Mode => In_File,
             Name => Filename (1 .. FN_Length));
-      Count := 1;
+      Count := 0;
       While not End_Of_File (Data_File) loop
+         Count := Count + 1;
          Ada.Text_IO.Get (File => Data_File, Item => Info(Count).Banner);
          Ada.Float_Text_IO.Get (File => Data_File, Item => Info(Count).GPA);
-         Ada.Text_IO.Get_Line (File => Data_File, Item => Info(Count).Name, Last => Info(Count).Name_Length);
-         Count := Count + 1;       
+         Ada.Text_IO.Get_Line (File => Data_File,
+                               Item => Info(Count).Name,
+                               Last => Info(Count).Name_Length);
       end loop;
    end Get_Students;
 
@@ -63,7 +69,19 @@ procedure Lastname_I_Lab_10 is
       Max_Name_Length : constant Integer := 20;
 
    begin -- Put_Students
-      put(Item => Info(Count).Banner);
+     for i in 1 .. Count loop
+      Ada.Text_IO.Put(Item => Info(i).Name);
+      for j in 1 .. Max_Name_Length - Info(i).Name_Length loop
+        Put(Item => " ");
+      end loop;
+      put(Item => Info(i).Banner);
+      put(Item => Info(i).GPA,
+          fore => 8,
+          aft => 2,
+          exp => 0);
+      New_Line;
+    end loop;
+
    end Put_Students;
 
    Student_Info : Student_Info_Type;
@@ -75,4 +93,4 @@ begin -- Lastname_I_Lab_10
    Print_Header;
    Put_Students (Info => Student_Info, Count => Student_Count);
 
-end Lastname_I_Lab_10;
+end moberg_c_lab_10;
